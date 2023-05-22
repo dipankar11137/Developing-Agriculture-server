cae212f73ee435a47aefc4a0da45115a56f452a4
Dipankar Halder <dipankar15-11137@diu.edu.bd>
2023-05-23 02:14:59 +0600

    buy product filterby email

--- index.js
+++ index.js
@@ -92,97 +92,14 @@ async function run() {
       const result = await buyProductsCollection.deleteOne(query);
       res.send(result);
     });
-    // const jobCollection = client.db('dailyNeeds').collection('jobs');
-    // const userCollection = client.db('dailyNeeds').collection('users');
-    // const allServicesCollection = client
-    //   .db('dailyNeeds')
-    //   .collection('services');
-    // const bookingCollection = client
-    //   .db('dailyNeeds')
-    //   .collection('bookingServices');
-
-    // // // // // // // // // // // // //
-
-    // // // post User  buyProduct
-    // app.post('/user', async (req, res) => {
-    //   const newProduct = req.body;
-    //   const result = await userCollection.insertOne(newProduct);
-    //   res.send(result);
-    // });
-    // // get User
-    // app.get('/users', async (req, res) => {
-    //   const query = {};
-    //   const cursor = userCollection.find(query);
-    //   const mainProducts = await cursor.toArray();
-    //   res.send(mainProducts);
-    // });
-
-    // //                    Jobs
-    // // get Jobs
-    // app.get('/jobs', async (req, res) => {
-    //   const query = {};
-    //   const cursor = jobCollection.find(query);
-    //   const jobs = await cursor.toArray();
-    //   res.send(jobs);
-    // });
-    // // post Jobs
-    // app.post('/jobs', async (req, res) => {
-    //   const newProduct = req.body;
-    //   const result = await jobCollection.insertOne(newProduct);
-    //   res.send(result);
-    // });
-    // // g
-
-    // // //                        ALl services
-    // // All Services Collection
-    // app.post('/allServices', async (req, res) => {
-    //   const newProduct = req.body;
-    //   const result = await allServicesCollection.insertOne(newProduct);
-    //   res.send(result);
-    // });
-
-    // // get all services
-    // app.get('/allServices', async (req, res) => {
-    //   const query = {};
-    //   const cursor = allServicesCollection.find(query);
-    //   const mainProducts = await cursor.toArray();
-    //   res.send(mainProducts);
-    // });
-    // // all service filter by service category
-    // app.get('/allServices/:service', async (req, res) => {
-    //   const service = req.params.service;
-    //   const query = { service };
-    //   const cursor = allServicesCollection.find(query);
-    //   const user = await cursor.toArray();
-    //   res.send(user);
-    // });
-    // // get all services by id
-    // app.get('/allServicesId/:id', async (req, res) => {
-    //   const id = req.params.id;
-    //   const query = { _id: ObjectId(id) };
-    //   const products = await productsCollection.findOne(query);
-    //   res.send(products);
-    // });
-    // // // Delete one Service
-    // app.delete('/allServices/:id', async (req, res) => {
-    //   const id = req.params.id;
-    //   const query = { _id: ObjectId(id) };
-    //   const result = await allServicesCollection.deleteOne(query);
-    //   res.send(result);
-    // });
-    // // post  book services
-    // app.post('/bookService', async (req, res) => {
-    //   const newProduct = req.body;
-    //   const result = await bookingCollection.insertOne(newProduct);
-    //   res.send(result);
-    // });
-    // // get Book Service
-    // app.get('/bookService', async (req, res) => {
-    //   const query = {};
-    //   const cursor = bookingCollection.find(query);
-    //   const mainProducts = await cursor.toArray();
-    //   res.send(mainProducts);
-    // });
+    // get buy by email
+    app.get('/buyProducts/:email', async (req, res) => {
+      const email = req.params.email;
+      const query = { email };
+      const cursor = buyProductsCollection.find(query);
+      const user = await cursor.toArray();
+      res.send(user);
+    });
   } finally {
   }
 }
