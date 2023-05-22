f439195d9b69826d0ff32aeda32ab35203e37ce7
Dipankar Halder <dipankar15-11137@diu.edu.bd>
2023-05-23 00:12:56 +0600

    add buy product

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const { ObjectId } = require('mongodb');
+var jwt = require('jsonwebtoken');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 
 const app = express();
@@ -11,8 +11,11 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.yow9vhz.mongodb.net/?retryWrites=true&w=majority`;
+
 const uri =
   'mongodb+srv://developing_agriculture:Ok7qWBeRv9jhBPTJ@cluster0.tkczuor.mongodb.net/?retryWrites=true&w=majority';
+
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -22,10 +25,14 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     await client.connect();
+    // console.log("database connect");
 
     const productsCollection = client
       .db('developingAgriculture')
       .collection('products');
+    const buyProductsCollection = client
+      .db('developingAgriculture')
+      .collection('buy');
 
     // get products
     app.get('/products', async (req, res) => {
@@ -33,6 +40,12 @@ async function run() {
       const cursor = productsCollection.find(query);
       const product = await cursor.toArray();
       res.send(product);
+    });
+    // post all products
+    app.post('/products', async (req, res) => {
+      const newProducts = req.body;
+      const result = await productsCollection.insertOne(newProducts);
+      res.send(result);
     });
     // all products filter by  category
     app.get('/product/:productsCategory', async (req, res) => {
@@ -42,27 +55,112 @@ async function run() {
       const item = await cursor.toArray();
       res.send(item);
     });
-    // all products filter by  id
-    // app.get('/productId/:_id', async (req, res) => {
-    //   const productsCategory = req.params._id;
-    //   const query = { _id };
-    //   const cursor = productsCollection.find(query);
-    //   const item = await cursor.toArray();
-    //   res.send(item);
+
+    // get all services by id
+    app.get('/buy/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const products = await productsCollection.findOne(query);
+      res.send(products);
+    });
+
+    // post Buy products
+    app.post('/buyProduct', async (req, res) => {
+      const newProducts = req.body;
+      const result = await buyProductsCollection.insertOne(newProducts);
+      res.send(result);
+    });
+    // const jobCollection = client.db('dailyNeeds').collection('jobs');
+    // const userCollection = client.db('dailyNeeds').collection('users');
+    // const allServicesCollection = client
+    //   .db('dailyNeeds')
+    //   .collection('services');
+    // const bookingCollection = client
+    //   .db('dailyNeeds')
+    //   .collection('bookingServices');
+
+    // // // // // // // // // // // // //
+
+    // // // post User  buyProduct
+    // app.post('/user', async (req, res) => {
+    //   const newProduct = req.body;
+    //   const result = await userCollection.insertOne(newProduct);
+    //   res.send(result);
     // });
-    // // get product by id
-    // app.get('/product/:id', async (req, res) => {
+    // // get User
+    // app.get('/users', async (req, res) => {
+    //   const query = {};
+    //   const cursor = userCollection.find(query);
+    //   const mainProducts = await cursor.toArray();
+    //   res.send(mainProducts);
+    // });
+
+    // //                    Jobs
+    // // get Jobs
+    // app.get('/jobs', async (req, res) => {
+    //   const query = {};
+    //   const cursor = jobCollection.find(query);
+    //   const jobs = await cursor.toArray();
+    //   res.send(jobs);
+    // });
+    // // post Jobs
+    // app.post('/jobs', async (req, res) => {
+    //   const newProduct = req.body;
+    //   const result = await jobCollection.insertOne(newProduct);
+    //   res.send(result);
+    // });
+    // // g
+
+    // // //                        ALl services
+    // // All Services Collection
+    // app.post('/allServices', async (req, res) => {
+    //   const newProduct = req.body;
+    //   const result = await allServicesCollection.insertOne(newProduct);
+    //   res.send(result);
+    // });
+
+    // // get all services
+    // app.get('/allServices', async (req, res) => {
+    //   const query = {};
+    //   const cursor = allServicesCollection.find(query);
+    //   const mainProducts = await cursor.toArray();
+    //   res.send(mainProducts);
+    // });
+    // // all service filter by service category
+    // app.get('/allServices/:service', async (req, res) => {
+    //   const service = req.params.service;
+    //   const query = { service };
+    //   const cursor = allServicesCollection.find(query);
+    //   const user = await cursor.toArray();
+    //   res.send(user);
+    // });
+    // // get all services by id
+    // app.get('/allServicesId/:id', async (req, res) => {
     //   const id = req.params.id;
     //   const query = { _id: ObjectId(id) };
     //   const products = await productsCollection.findOne(query);
     //   res.send(products);
     // });
-    // post all products
-    app.post('/products', async (req, res) => {
-      const newProducts = req.body;
-      const result = await productsCollection.insertOne(newProducts);
-      res.send(result);
-    });
+    // // // Delete one Service
+    // app.delete('/allServices/:id', async (req, res) => {
+    //   const id = req.params.id;
+    //   const query = { _id: ObjectId(id) };
+    //   const result = await allServicesCollection.deleteOne(query);
+    //   res.send(result);
+    // });
+    // // post  book services
+    // app.post('/bookService', async (req, res) => {
+    //   const newProduct = req.body;
+    //   const result = await bookingCollection.insertOne(newProduct);
+    //   res.send(result);
+    // });
+    // // get Book Service
+    // app.get('/bookService', async (req, res) => {
+    //   const query = {};
+    //   const cursor = bookingCollection.find(query);
+    //   const mainProducts = await cursor.toArray();
+    //   res.send(mainProducts);
+    // });
   } finally {
   }
 }
@@ -70,9 +168,9 @@ async function run() {
 run().catch(console.dir);
 
 app.get('/', (req, res) => {
-  res.send('Running development-agriculture-e-serve ');
+  res.send('Running Developing Agriculture ');
 });
 
 app.listen(port, () => {
-  console.log('development-agriculture-e-serve  server is running ');
+  console.log('Developing Agriculture  server is running ');
 });
